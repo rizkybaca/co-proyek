2c424c708eb31010971d4329b3f8a18078c88222
ta <akun.rizky.baca@gmail.com>
2021-04-24 12:26:41 +0700

    ubah struktur index dg flex sekaligus sinkron css

--- index.php
+++ index.php
@@ -2,52 +2,45 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1,0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- link style -->
 	<link rel="stylesheet" type="text/css" href="./dist/css/style.css">
 	<!-- tittle -->
 	<title>Captain Order</title>
 </head>
+
 <body>
-<aside>	
-	<nav>
-		<ul>
-			<li>
-				<a href="#">
-					<i>a</i>
-					<p>Stores</p>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<i>a</i>
-					<p>Client-Request</p>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<i>a</i>
-					<p>End-User List</p>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<i>a</i>
-					<p>Settings</p>
-				</a>
-			</li>
-		</ul>
-	</nav>
-	<div class="logout">
-		<ul>
-			<li>
-				<a onclick="return confirm('Apakah Anda yakin akan keluar?')" href="logout.php">
-					<i>a</i>
-					<p>Logout</p>
-				</a>
-			</li>
-		</ul>
+
+<header>
+	Captain Order
+</header>
+
+<div class="container">
+	<aside>	
+		<nav>
+			<a href="#">
+				<i>a</i>Stores
+			</a>
+			<a href="#">
+				<i>a</i>Client-Request
+			</a>
+			<a href="#">
+				<i>a</i>End-User List
+			</a>
+			<a href="#">
+				<i>a</i>Settings
+			</a>
+		</nav>
+		<div class="logout">
+			<a onclick="return confirm('Apakah Anda yakin akan keluar?')" href="logout.php">
+				<i>a</i>Logout
+			</a>
+		</div>
+	</aside>
+	<div class="content">
+		di sini kontennya
 	</div>
-</aside>
+</div>
+
 </body>
 </html>
