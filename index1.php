6d35bb7295ec44844c51cfc7d99864ba1c545393
ta <akun.rizky.baca@gmail.com>
2021-05-02 11:40:25 +0700

    update index1 dan css

--- index1.php
+++ index1.php
@@ -56,9 +56,49 @@
 				</div>
 			</div>
 			<div class="cont">
-				<div class="store">
-					
-				</div>
+				<a href="">
+					<div class="store">
+						<div class="image"></div>
+						<div class="desc">
+							<div class="tittle">
+								<p>Yamie Panda Chapter Monjali</p>
+							</div>
+							<div class="sub">
+								<p>Jl. Diponegoro No. 12, Mlati, Sleman</p>
+							</div>
+							<div class="button"></div>
+						</div>
+					</div>
+				</a>
+				<a href="">
+					<div class="store">
+						<div class="image"></div>
+						<div class="desc">
+							<div class="tittle">
+								<p>Chingu Cafe Jogja</p>
+							</div>
+							<div class="sub">
+								<p>Jl. Mulawarman, Umbulharjo, Yogyakarta</p>
+							</div>
+							<div class="button"></div>
+						</div>
+					</div>
+				</a>
+				<a href="">
+					<div class="store">
+						<div class="image"></div>
+						<div class="desc">
+							<div class="tittle">
+								<p>Chingu Cafe Jogja</p>
+							</div>
+							<div class="sub">
+								<p>Jl. Mulawarman, Umbulharjo, Yogyakarta a a a a a a a a a a a a  a a a </p>
+							</div>
+							<div class="button"></div>
+						</div>
+					</div>
+				</a>
+			</div>
 		</div>
 	</div>
 </body>
