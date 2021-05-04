cc91adf3ab498e2c0cec7c39cc0ddd47a31ee0cf
ta <akun.rizky.baca@gmail.com>
2021-05-04 14:21:47 +0700

    layouting add stores

--- super/stores/add.php
+++ super/stores/add.php
@@ -11,16 +11,30 @@
 				<label for="name">Name</label>
 				<input type="text" name="name" id="name" placeholder="type name here" required>
 			</div>
-			<label for="address">Address</label>
-			<textarea id="address" placeholder="type address here" required></textarea>
-			<label for="client_name">Client Name</label>
-			<input type="text" name="client_name" id="client_name" placeholder="type client name here" required>
-			<label for="contact">Contact</label>
-			<input type="text" name="contact" id="contact" placeholder="type contact here" required>
-			<label for="email">E-mail</label>
-			<input type="text" name="email" id="email" placeholder="type e-mail here" required>
-			<label for="image">Upload store image here</label>
-			<input type="file" name="image">
+			<div class="col">
+				<label for="address">Address</label>
+				<textarea id="address" placeholder="type address here" required></textarea>
+			</div>
+			<div class="col">
+				<label for="client_name">Client Name</label>
+				<input type="text" name="client_name" id="client_name" placeholder="type client name here" required>
+			</div>
+			<div class="col">
+				<label for="contact">Contact</label>
+				<input type="text" name="contact" id="contact" placeholder="type contact here" required>
+			</div>
+			<div class="col">
+				<label for="email">E-mail</label>
+				<input type="text" name="email" id="email" placeholder="type e-mail here" required>
+			</div>
+			<div class="col2">
+				<label for="image">Upload store image here</label>
+				<input type="file" name="image">
+			</div>
+		</div>
+		<div class="button">
+			<a href="">Cancel</a>
+			<input type="submit" name="save" value="Save">
 		</div>
 	</form>
 </div>
