77c50ba6e7ea7e0dec114189e72c67a6ebf2004b
ta <akun.rizky.baca@gmail.com>
2021-05-24 18:34:35 +0700

    db

--- db/db_co (1).sql
+++ db/db_co (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Bulan Mei 2021 pada 16.54
+-- Waktu pembuatan: 24 Bulan Mei 2021 pada 13.33
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.3.27
 
@@ -35,10 +35,17 @@ CREATE TABLE `client` (
   `address` varchar(100) NOT NULL,
   `phone_number` varchar(13) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `id_card` varchar(20) NOT NULL,
-  `profile_picture` varchar(100) NOT NULL,
-  `business_license` varchar(100) NOT NULL
+  `id_card` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) NOT NULL,
+  `business_license` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `client`
+--
+
+INSERT INTO `client` (`id`, `username`, `password`, `name_c`, `address`, `phone_number`, `email`, `id_card`, `profile_picture`, `business_license`) VALUES
+(32, 'mama', 'mama1', 'mama', 'segsdg', '436', 'xdgbsd@1', '827649583_bami.jpg', '243672154_MBAK.jpeg', '418684420_alip.jpg');
 
 -- --------------------------------------------------------
 
@@ -61,7 +68,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `username`, `password`, `name`, `join_date`, `email`, `phone_number`) VALUES
-(7, 'afi', 'afi1!', 'afiva', '5 Januari 2012', 'afiv@gmail', '123141');
+(15, 'nana', 'nana1', 'nana nih', 'ini tanggal', 'nana@a', '35252');
 
 -- --------------------------------------------------------
 
@@ -73,9 +80,18 @@ CREATE TABLE `detail_order` (
   `id` int(11) NOT NULL,
   `id_order` int(11) NOT NULL,
   `id_product` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
+  `cont` int(11) NOT NULL,
   `notes` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_order`
+--
+
+INSERT INTO `detail_order` (`id`, `id_order`, `id_product`, `cont`, `notes`) VALUES
+(29, 18, 9, 1, 'ini order'),
+(30, 19, 9, 5, 'ini order'),
+(31, 19, 10, 1, 'ini order');
 
 -- --------------------------------------------------------
 
@@ -85,12 +101,20 @@ CREATE TABLE `detail_order` (
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `date` varchar(100) NOT NULL,
-  `period` varchar(100) NOT NULL,
+  `date_o` varchar(100) NOT NULL,
   `total` int(11) NOT NULL,
-  `id_end-user` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `status` enum('bb','sb','ex','cc') NOT NULL,
   `id_cashier` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `orders`
+--
+
+INSERT INTO `orders` (`id`, `date_o`, `total`, `id_customer`, `status`, `id_cashier`) VALUES
+(18, 'ini tanggal', 12000, 15, 'sb', 7),
+(19, 'ini tanggal', 65000, 15, 'bb', 0);
 
 -- --------------------------------------------------------
 
@@ -99,14 +123,23 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+  `id_p` int(11) NOT NULL,
   `types` enum('foods','drinks','snacks') NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `name_p` varchar(50) NOT NULL,
   `price` int(11) NOT NULL,
   `stocks` int(11) NOT NULL,
-  `picture` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `id_store` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id_p`, `types`, `name_p`, `price`, `stocks`, `image`, `id_store`) VALUES
+(9, 'foods', 'nasi uduk', 12000, 100, '984585729_nasiuduk@.jpg', 22),
+(10, 'drinks', 'coca cola', 5000, 100, '1159428163_cocacola@.jpg', 22),
+(11, 'snacks', 'french fries', 12000, 100, '313324949_frenchfrise@.jpg', 22);
 
 -- --------------------------------------------------------
 
@@ -116,12 +149,21 @@ CREATE TABLE `products` (
 
 CREATE TABLE `servant` (
   `id` int(11) NOT NULL,
-  `username` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `password` varchar(8) NOT NULL,
   `name` varchar(50) NOT NULL,
   `join_date` varchar(100) NOT NULL,
-  `role` enum('administrator','cashier') NOT NULL
+  `role` enum('product_admin','cashier') NOT NULL,
+  `id_store_v` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `servant`
+--
+
+INSERT INTO `servant` (`id`, `username`, `password`, `name`, `join_date`, `role`, `id_store_v`) VALUES
+(6, 'fafa', 'fafa1', 'fafa aja lah', 'segsd', 'product_admin', 22),
+(7, 'kaka', 'kaka1', 'kaka', 'drfhdrg', 'cashier', 22);
 
 -- --------------------------------------------------------
 
@@ -138,6 +180,13 @@ CREATE TABLE `store` (
   `id_client` int(11) NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `store`
+--
+
+INSERT INTO `store` (`id`, `name`, `address`, `phone_number`, `status`, `id_client`, `image`) VALUES
+(22, 'wang', 'fafa', '4634', 'active', 32, '1945494528_mcd@.jpg');
 
 -- --------------------------------------------------------
 
@@ -180,29 +229,30 @@ ALTER TABLE `customer`
 --
 ALTER TABLE `detail_order`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_order` (`id_order`),
-  ADD KEY `id_product` (`id_product`);
+  ADD KEY `id_product` (`id_product`),
+  ADD KEY `id_order` (`id_order`);
 
 --
 -- Indeks untuk tabel `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `orders_ibfk_1` (`id_cashier`),
-  ADD KEY `id_end-user` (`id_end-user`);
+  ADD KEY `id_cashier` (`id_cashier`),
+  ADD KEY `id_customer` (`id_customer`);
 
 --
 -- Indeks untuk tabel `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_p`),
   ADD KEY `id_store` (`id_store`);
 
 --
 -- Indeks untuk tabel `servant`
 --
 ALTER TABLE `servant`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_store_v` (`id_store_v`);
 
 --
 -- Indeks untuk tabel `store`
@@ -225,43 +275,43 @@ ALTER TABLE `super_user`
 -- AUTO_INCREMENT untuk tabel `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_order`
 --
 ALTER TABLE `detail_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT untuk tabel `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `servant`
 --
 ALTER TABLE `servant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `store`
 --
 ALTER TABLE `store`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `super_user`
@@ -277,21 +327,26 @@ ALTER TABLE `super_user`
 -- Ketidakleluasaan untuk tabel `detail_order`
 --
 ALTER TABLE `detail_order`
-  ADD CONSTRAINT `detail_order_ibfk_1` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_order_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `detail_order_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_p`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `detail_order_ibfk_3` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_cashier`) REFERENCES `servant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`id_end-user`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_store`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `servant`
+--
+ALTER TABLE `servant`
+  ADD CONSTRAINT `servant_ibfk_1` FOREIGN KEY (`id_store_v`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `store`
